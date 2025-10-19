60a676a198737c05805bdae2e21f6fd438b17fa8
Hunde Tolera <hundetolera225@gmail>
2025-10-19 17:21:24 +0300

    Add task_4.sql to print full description of Books table

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+-- task_4.sql
+-- Script to print full description of the table 'Books' in 'alx_book_store'
+
+SELECT 
+    COLUMN_NAME AS `Column`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Nullable`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE()  -- This will use the database passed as argument
+    AND TABLE_NAME = 'Books';
